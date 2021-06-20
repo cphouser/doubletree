63a778265206831b83e213555e05b8e7d8cfd885
Calvin Houser <cphouser@gmail.com>
2021-06-20 16:50:31 -0700

    add defs for file accessed time property

--- src/init.pl
+++ src/init.pl
@@ -7,13 +7,6 @@
 
 :- rdf_load('../data/vocab/xcat.rdfs').
 
-%:- rdf_load('../data/triples.rdf').
-%:- rdf_load_db('../data/pl_store/triples.db').
 :- rdf_attach_db('../data/pl_store', [access(read_write)]).
-%:- rdf_default_graph(_, '/home/xeroxcat/projects/doubletree/data/triples.rdf').
-%:- rdf_default_graph(_, 'file:///home/xeroxcat/projects/doubletree/data/triples.rdf').
-%:- rdf_default_graph(_, '../data/triples.rdf').
-%
-%:- rdf_save_db('../data/some_out.db').
-%:- rdf_save_db('../data/pl_store/triples.db').
+
 :- rdf_register_prefix(xcat, 'http://xeroxc.at/schema#').
